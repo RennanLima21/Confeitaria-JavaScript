4898cc46c6dbb1bbadf531037cde0a8fb2262b45
Rennan Lima <rennanlima@hotmail.com>
2023-02-27 15:29:44 -0400

    orders and preparations

--- index.js
+++ index.js
@@ -7,17 +7,50 @@ const prompt = require('prompt-sync')({sigint: true})
 /*
 RECEBER O PEDIDO
 */
+function recebePedido (){
+  let pedido = {
+      sabor: null,
+      confeitos: null
+
+  }
   // Qualo sabor do Bolo?
+  pedido.sabor = prompt('Qual o sabor que gostaria? ')
   // Quais confeitos deseja?
+  pedido.confeitos = prompt('Quais são os confeitos? ')
+
+    return pedido
+}
+
 
 /* 
 PREPARAR O PEDIDO 
 */
+function preparaPedido(pedido){
   // Separar Igredientes
+    let ingredientes_base = separaIngredientes(pedido)
   // Misturar o bolo
+   let tabuleiro = misturaBolo(pedido, ingredientes_base)
   // Colocar no forno
+   let bolo = colocaForno(tabuleiro)
   // Confeitar
+  bolo = confeitar(pedido, bolo)
 
+return bolo
+}
+// FUNCTIONS DOS PROCESSOS
+function separaIngredientes(pedido){
+    let ingredientes_base = 'ovos, leite, farinaha, fermento,'
+    return ingredientes_base
+}
+function misturaBolo(pedido, ingredientes_base){
+    return (pedido.sabor + ', ' + ingredientes_base)
+}
+function colocaForno(tabuleiro){
+    return 'Bolo de  ' + tabuleiro
+}
+function confeitar(pedido, bolo){
+  return bolo + ' confeitado com ' + pedido.confeitos
+}
 /* 
 CALCULAR O PREÇO 
 */
@@ -39,3 +72,17 @@ RECEBER O DINHEIRO
 */
   // Receber dinheiro do cliente
   // Fechar pedido e agradecer o cliente
+
+
+
+
+
+
+
+
+
+let pedido = recebePedido()
+let bolo = preparaPedido(pedido)
+
+console.log(pedido)
+console.log(bolo)
