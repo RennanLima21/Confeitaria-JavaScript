f8a08fd2f679d7f8c6e6a49a0366e9ecd482db36
Rennan Lima <rennanlima@hotmail.com>
2023-02-27 15:01:50 -0400

    check list

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+Biblioteca para capturar inpust de texto do usuário no console
+*/
+const prompt = require('prompt-sync')({sigint: true})
+
+
+/*
+RECEBER O PEDIDO
+*/
+  // Qualo sabor do Bolo?
+  // Quais confeitos deseja?
+
+/* 
+PREPARAR O PEDIDO 
+*/
+  // Separar Igredientes
+  // Misturar o bolo
+  // Colocar no forno
+  // Confeitar
+
+/* 
+CALCULAR O PREÇO 
+*/
+  // Valor dos ingredientes
+  // Valor da confeiteira
+  // Valor das despesas de produção
+  // Valor dos gastos fixos
+  // Valor do lucro
+  // Retornar preço final
+  
+/* 
+ENTREGAR O PEDIDO 
+*/
+  // Exibir / entregar bolo do cliente
+  // Exibir preço para o cliente
+
+/* 
+RECEBER O DINHEIRO 
+*/
+  // Receber dinheiro do cliente
+  // Fechar pedido e agradecer o cliente
